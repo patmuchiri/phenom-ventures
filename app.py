bb57968716edbac735204cca69009d584b785bdf
patmuchiri <pmuchirig@gmail.com>
2024-09-12 07:38:45 +0300

    removed errors

--- app.py
+++ app.py
@@ -3,29 +3,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 application = app
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
 @app.route('/about')
 def about():
     return render_template('about.html')
+
 
 @app.route('/services')
 def services():
     return render_template('services.html')
 
+
 @app.route('/portfolio')
 def portfolio():
     return render_template('portfolio.html')
 
+
 @app.route('/testimonials')
 def testimonials():
     return render_template('testimonials.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
